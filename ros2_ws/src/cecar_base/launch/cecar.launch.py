15ad1c6b32eb3d8b53add84856200235b74ec90c
jass <philipp.jass@htw-berlin.de>
2021-03-09 12:32:19 +0100

    Renamed package cecar -> cecar_base and modified launch files in cecar_base.

---
+++ ros2_ws/src/cecar_base/launch/cecar.launch.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+#Launch file for CeCar as part of my Bachelor's paper
+#Author(s)		: Lukas Mirow
+#Date of creation	: 7/27/2020
+#Modified for parameterization of namespace: Philipp Jass (3/9/2021)
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+	config = os.path.join(
+	get_package_share_directory('cecar_base'),
+	'launch',
+	'parameters.yaml'
+	),
+
+	return LaunchDescription([
+		#Define launch arguments for parameterization of launchConfiguration from command line                         
+        DeclareLaunchArgument('ns', description='Namespace to launch nodes in.'),
+
+		Node(
+			package='cecar_base',
+			node_namespace=LaunchConfiguration('ns'),
+			node_executable='drive',
+			output='screen'
+		),
+
+		Node(
+			package='cecar_base',
+			node_namespace=LaunchConfiguration('ns'),
+			node_executable='self_protection',
+			output='screen'
+		),
+
+		Node(
+			package='cecar_base',
+			node_namespace=LaunchConfiguration('ns'),
+			node_executable='self_control',
+			output='screen'
+		),
+
+		Node(
+			package='cecar_base',
+			node_namespace=LaunchConfiguration('ns'),
+			node_executable='intelligence_collection',
+			output='screen'
+		),
+
+		Node(
+			package='cecar_base',
+			node_namespace=LaunchConfiguration('ns'),
+			node_executable='rcu_comm',
+			output='screen',
+			parameters=[config]
+		)
+
+	])
---
+++ ros2_ws/src/cecar_base/launch/rc.launch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+#Remote control launch file for CeCar as part of my Bachelor's paper
+#Author(s)		: Lukas Mirow
+#Date of creation	: 7/27/2020
+#Modified for parameterization of namespace by Philipp Jass (3/9/2021)
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+	return LaunchDescription([
+		#Define launch arguments for parameterization of launchConfiguration from command line                         
+        DeclareLaunchArgument('ns', description='Namespace to launch nodes in.')
+
+		Node(
+			package='cecar_base',
+			node_executable='gamepad_rc',
+			node_namespace=LaunchConfiguration('ns'),   #use launch argument for setting node namespace,
+			output='screen'	#change to 'log' to receive node output in a log-file
+		)
+	])

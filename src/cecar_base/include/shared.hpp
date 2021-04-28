39b0ac9d178053adfd92578dbabc72f4f2b6d5ad
s0558009 <lukas.mirow@student.htw-berlin.de>
2021-04-28 08:23:15 +0200

    Added missing ',' to remote control launch file, yet antoher attempt to fix submodules, and added build and editor files to .gitignore

---
+++ src/cecar_base/include/shared.hpp
@@ -0,0 +1,30 @@
+/*Shared header file for CeCar
+Author(s)		: Lukas Mirow
+Date of creation	: 3/13/2020
+*/
+
+#ifndef CECAR_SHARED
+#define CECAR_SHARED
+
+#define MSG_BUFFER_SIZE 5
+#define DRIVE_INPUT_MSG_DELAY 5ms
+#define DRIVE_OUTPUT_MSG_DELAY 3 * DRIVE_INPUT_MSG_DELAY
+#define WHEEL_ENCODER_MSG_DELAY 100ms
+#define DRIVE_SPEED_AMPLITUDE_FORWARDS 0.7992
+#define DRIVE_SPEED_AMPLITUDE_BACKWARDS 0.999
+#define DRIVE_STEERING_AMPLITUDE 0.9
+#define UART_SPEED B57600
+#define UART_PATH "/dev/ttyTHS2"
+#define SAFE_DRIVING_THROTTLE 0.1
+#define STEERING_OFFSET_STEP 0.1
+
+#include "rclcpp/rclcpp.hpp"
+#include "cecar_msgs/msg/ackermann_drive.hpp"
+#include "cecar_msgs/msg/wheel_enc.hpp"
+#include "cecar_msgs/msg/cecar_status.hpp"
+
+typedef cecar_msgs::msg::AckermannDrive Ackermann_drive;
+typedef cecar_msgs::msg::WheelEnc Wheel_encoder_cecar;
+typedef cecar_msgs::msg::CecarStatus Cecar_status;
+
+#endif //CECAR_SHARED
